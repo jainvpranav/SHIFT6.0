4cdb8637e590fd599ba7c6a367e49f94b6d4d971
Pranav V <100434347+jainvpranav@users.noreply.github.com>
2023-11-18 21:34:04 +0530

    Add files via upload

---
+++ static/js/index.js
@@ -0,0 +1,218 @@
+const chatInput = document.querySelector(".chat-input");
+const sendButton = document.querySelector(".send-btn");
+const chatContainer = document.querySelector(".chat-container");
+const deleteButton = document.querySelector(".delete-btn");
+const themeButton = document.querySelector(".theme-btn");
+
+
+const loadDataFromLocalStorage = () => {
+    const themeColor = localStorage.getItem("themeColor");
+    document.body.classList.toggle("light-mode", themeColor === "light_mode");
+    themeButton.innerHTML = document.body.classList.contains("light-mode")
+        ? `<img src="static/assets/night.png" alt="Dark Mode" width="30px" height="30px" >`
+        : `<img src="static/assets/sun.png" alt="Light Mode" width="30px" height="30px" >`;
+
+    const defaultText = `<div class="default-text">
+                                <h1>Shift</h1>
+                                <h3>Explore the world of Fashion.</h3>
+                            </div>`;
+
+    chatContainer.innerHTML = localStorage.getItem("all-chats") || defaultText;
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+};
+
+const createChatElement = (content, className) => {
+    const chatDiv = document.createElement("div");
+    chatDiv.classList.add("chat", className);
+    chatDiv.innerHTML = content;
+    return chatDiv;
+};
+
+const handleOutgoingChat = async () => {
+    let UserText = chatInput.value.trim();
+    if (!UserText) return;
+
+    chatInput.value = "";
+    chatInput.style.height = `${initialInputHeight}px`;
+
+    const html = `<div class="chat-content">
+                        <div class="chat-details">
+                            <img src="static/assets/user.png" alt="user-img">
+                            <p>${UserText}</p>
+                        </div>
+                    </div>`;
+
+    const outChatDiv = createChatElement(html, "outgoing");
+    chatContainer.querySelector(".default-text")?.remove();
+    chatContainer.appendChild(outChatDiv);
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+    await request(UserText);
+    setTimeout(showTypingAnimation, 500);
+};
+
+const showTypingAnimation = () => {
+    const html = `<div class="chat-content">
+                    <div class="chat-details">
+                        <img src="static/assets/designer.png" alt="chatbot-img">
+                        <div class="typing-animation">
+                            <div class="typing-dot" style="--delay: 0.2s"></div>
+                            <div class="typing-dot" style="--delay: 0.3s"></div>
+                            <div class="typing-dot" style="--delay: 0.4s"></div>
+                        </div>
+                    </div>
+                </div>`;
+    const inChatDiv = createChatElement(html, "incoming");
+    chatContainer.appendChild(inChatDiv);
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+    setTimeout(response1(inChatDiv), 1000);
+};
+
+// <span onclick="copyResponse(this)" class="material-symbols-rounded">content_copy</span>
+
+// const copyResponse = (copyBtn) => {
+//     const responseTextElement = copyBtn.parentElement.querySelector("p");
+//     navigator.clipboard.writeText(responseTextElement.textContent);
+//     copyBtn.textContent = "done";
+//     setTimeout(() => (copyBtn.textContent = "content_copy"), 1000);
+// };
+
+deleteButton.addEventListener("click", () => {
+    // if (confirm("Are you sure you want to delete all the chats?")) {
+    localStorage.removeItem("all-chats");
+    loadDataFromLocalStorage();
+    // }
+});
+
+
+themeButton.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+    localStorage.setItem("themeColor", themeButton.innerText);
+    themeButton.innerHTML = document.body.classList.contains("light-mode")
+        ? `<img src="static/assets/night.png" alt="Dark Mode" width="30px" height="30px" >`
+        : `<img src="static/assets/sun.png" alt="Light Mode" width="30px" height="30px" >`;
+});
+
+const initialInputHeight = chatInput.scrollHeight;
+
+chatInput.addEventListener("input", () => {
+    chatInput.style.height = `${initialInputHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleOutgoingChat();
+    }
+});
+
+loadDataFromLocalStorage();
+sendButton.addEventListener("click", handleOutgoingChat);
+
+function generateImages(textPrompt, inChatDiv) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/generate_images", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var data=xhr.responseText;
+            var jsonResponse = JSON.parse(data);
+            imageviewer(inChatDiv,jsonResponse["image_filenames"][0],jsonResponse["image_filenames"][1],jsonResponse["image_filenames"][2]);
+        } else {
+            alert("Error generating images. Please try again.");
+        }
+    };
+
+    xhr.send(JSON.stringify({ textPrompt: textPrompt }));
+}
+
+// const openImage = (a) => {
+//     console.log(a);
+//     document.getElementById("image-popup").src = a.src || a;
+//     document.getElementById("image-container").style.display = "flex";
+// } 
+
+// const closeImg = () => {
+//     document.getElementById("image-container").style.display = "none";
+// }
+
+const imageviewer = (inChatDiv,a,b,c) => {
+    alert("Images generated successfully!");
+    let html = `<div class="images">
+                    <img src="${a}" class="thumbnail" alt="img1">
+                    <img src="${b}" class="thumbnail" alt="img2">
+                    <img src="${c}" class="thumbnail" alt="img3">
+                </div>`
+    const imagediv = createChatElement(html, "imagesdiv");
+    inChatDiv.querySelector(".chat-details").appendChild(imagediv);
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let databrowser = "";
+const request = async (prompt) => {
+    const payload = {
+        prompt: {
+            text: `Extract essential parameters from ${prompt} for precise, high-quality, full-size fashion images using Vertex AI Vision on Google Cloud Platform. Prioritize details, avoid unrelated text, and use only text directly contributing to image generation. Instruct the model to prioritize quick and effective dress generation while maintaining quality.Get all this data in 2 lines & produce only sentence & no other punctutaions.`
+        }
+    };
+    // Define the URL for the Google Cloud Language API
+    const url = "https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=AIzaSyC51hnGqn_3ge9VVLNGoZO0i1blez-7ONA";
+    // Make the POST request
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+    })
+        .then(response => response.json())
+        .then(data => {
+            databrowser = data.candidates[0].output;
+            console.log("databrowser", databrowser);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+};
+
+const response1 = (inChatDiv) => {
+    try {
+        console.log(databrowser);
+        inChatDiv.querySelector(".typing-animation").remove();
+        const message = databrowser;
+        const messageElement = document.createElement("p");
+        messageElement.textContent = message;
+        inChatDiv.querySelector(".chat-details").appendChild(messageElement);
+        generateImages(message, inChatDiv);
+    }
+    catch (error) {
+        inChatDiv.querySelector(".typing-animation").remove();
+        const errorMessage = "Sorry, I can't assist you with this.";
+        const errorElement = document.createElement("p");
+        errorElement.textContent = errorMessage;
+        inChatDiv.querySelector(".chat-details").appendChild(errorElement);
+        console.log(error);
+    }
+};
